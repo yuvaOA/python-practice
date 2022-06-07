0fd50bde237542c29013ee5eca8b44a9f32596c8
yuvaOA <107019690+yuvaOA@users.noreply.github.com>
2022-06-07 21:38:44 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,14 @@
+#creating a leave letter using python
+
+print("from:")
+print("\txxx")
+print("\tyyy")
+print("to:")
+print("\tocean academy,")
+print("\t45 feet road,")
+print("\tpondicherry.")
+print("respected sir:")
+print("\t\tas I am suffering from fever I could not able to attend the class for three days.")
+print("                                  thanking you                                ")
+print("yours faithfully,")
+print("      xxx        ")
