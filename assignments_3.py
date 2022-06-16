adf607fd134386051467bca70355efc132294a8e
yuvaOA <107019690+yuvaOA@users.noreply.github.com>
2022-06-16 20:16:42 +0530

    Add files via upload

---
+++ assignments_3.py
@@ -0,0 +1,17 @@
+#area of rectangle
+l=int(input("Enter the value of l:"))
+b=int(input("Enter the value of b:"))
+print("area of rectangle=",l*b)
+
+#area of square
+a=int(input("Enter the value of a:"))
+print("area of square=",4*a)
+
+#area of triangle
+b=int(input("Enter the value of b:"))
+h=int(input("Enter the value of h:"))
+print("area of triangle=",1/2*b**b*h)
+
+#area of circle
+r=int(input("Enter the value of r:"))
+print("area of circle=",22/7*r*r)
